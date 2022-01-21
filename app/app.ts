57ccb44fcf3aeecf9ca22b047cf4b43ee4a0bfb8
victordantasdev <victordantasdeveloper@gmail.com>
2022-01-20 21:42:03 -0300

    C2 M5

--- app/app.ts
+++ app/app.ts
@@ -4,10 +4,14 @@ import { NegociacoesView } from './views/negociacoes-view.js';
 const controller = new NegociacaoController;
 const form = document.querySelector('.form');
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-  controller.adiciona();
-})
+if (form) {
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    controller.adiciona();
+  });
+} else {
+  throw Error('Não foi possível iniciar a aplicação. Verifique se o form existe!'); 
+}
 
 // const negociacoesView = new NegociacoesView();
 // const template = negociacoesView.template();
